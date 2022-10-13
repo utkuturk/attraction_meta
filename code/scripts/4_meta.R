402d1361c2554ee9334ce2b5d2878504171c84b5
utku turk <utkuturk@umd.edu>
2022-10-13 15:03:16 -0400

    gitignore relieved

--- code/scripts/4_meta.R
+++ code/scripts/4_meta.R
@@ -3,13 +3,8 @@ fname_data = "data/data.rds"
 fname_p = "data/p_avg.RData"
 fname_pm = "data/model_plots.RData"
 fname_m = "data/models.RData"
-library(gdata)
-library(magrittr)
-library(purrr)
-library(dplyr)
-library(ggplot2)
-library(ggsci)
-library(brms)
+library(gdata); library(magrittr); library(purrr)
+library(dplyr); library(ggplot2); library(ggsci); library(brms)
 select = dplyr::select
 invisible(lapply(list.files("code/util/", full.names = T), source))
 data = readRDS(fname_data)
@@ -72,7 +67,7 @@ m$meta = brm(
 )
 
 pm$meta = brms_plot(
-  m_meta, exclude_names = "l_trial",
+  m$meta, exclude_names = "l_trial",
   plot_stats = T, map_names = mycoefs,
   expand_right = 1, expand_top = 4,
   x_stat_adjust = 0.4, x_breaks = seq(-1.5, 2.5, 0.5)
@@ -86,7 +81,7 @@ saveRDS(pm, "data/model_plots.RData")
 # Interaction-plot
 
 summaries = map(m, model_summary) %>%
-  map(rownames_to_column) %>%
+  map(tibble::rownames_to_column) %>%
   map(filter, rowname=="bias:c_mis")
 summaries$tl1$rowname = "Turk & Logacev (Exp1)"
 summaries$tl2b$rowname = "Turk & Logacev (Exp2)"
@@ -121,7 +116,7 @@ ggsave("data/figures/meta.png", plot=last_plot(), device="png", dpi="retina", wi
 
 
 summaries = map(m, model_summary) %>%
-  map(rownames_to_column) %>%
+  map(tibble::rownames_to_column) %>%
   map(filter, rowname=="c_mis")
 summaries$tl1$rowname = "Turk & Logacev (Exp1) Plurality"
 summaries$tl2b$rowname = "Turk & Logacev (Exp2) Plurality"
